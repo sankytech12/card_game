c814c10f5aa2d904185cc4cbaa0976ba3a523a68
SASANK SAH <sasanksah@SASANKs-MacBook-Pro.local>
2023-04-12 16:09:11 +0530

    Comments added

--- src/main/Card.java
+++ src/main/Card.java
@@ -1,39 +1,40 @@
 package main;
 
-
-
 public class Card {
 
-    private int card_number;
-    private Suits current_suit;
+	private int card_number;
+	private Suits current_suit;
 
-    public void set_card_number(int number) {
-        this.card_number = number;
-    }
+	public void set_card_number(int number) { // Method to set the card number
+		this.card_number = number;
+	}
 
-    public void set_current_suit(Suits suit) {
-        this.current_suit = suit;
-    }
+	public void set_current_suit(Suits suit) { // Method to set current suit
+		this.current_suit = suit;
+	}
 
-    public int get_card_number() {
-        return card_number;
-    }
-    public Suits get_current_suit() {
-        return current_suit;
-    }
+	public int get_card_number() { // Method to get the card number
+		return card_number;
+	}
 
-    Card(){
+	public Suits get_current_suit() { // Method to get current suit
+		return current_suit;
+	}
 
-    }
+	// Default Constructor
+	Card() {
 
-    public Card(int num, Suits s) {
-        card_number = num;
-        current_suit = s;
-    }
-    
-    @Override
-    public String toString() {
-        return card_number + " " + current_suit;
-    }
+	}
+
+	// Card constructor with card number and suit name as parameters
+	public Card(int num, Suits s) {
+		card_number = num;
+		current_suit = s;
+	}
+
+	@Override
+	public String toString() {
+		return card_number + " " + current_suit;
+	}
 
 }
--- src/main/Player.java
+++ src/main/Player.java
@@ -5,28 +5,38 @@ import java.util.ArrayList;
 public class Player {
 
 	private int current_id;
-	private ArrayList<Card> current_cards = new ArrayList<>();
 
-	public Player(int id) {
+	private ArrayList<Card> current_cards = new ArrayList<>(); // cards in players hand
+
+	public Player(int id) { // Player constructor having player's id as parameter
 		this.current_id = id;
 	}
 
-	public void add_card(Card card) {
+	/*
+	 * This method is for adding card to the player's hand after if the card is not
+	 * matched with the top of discard pile or after any action card (Q,J) played by
+	 * the previous player.
+	 */
 
+	public void add_card(Card card) {
 		current_cards.add(card);
 	}
- 
-	
+
+	/*
+	 * This method remove the card from the player's hand when any of the player's
+	 * card matches with the top of the discard pile.
+	 */
+
 	public void remove_card(Card card) {
 		current_cards.remove(card);
 		return;
 	}
 
-	public int return_id() {
+	public int return_id() { // To get the player id
 		return current_id;
 	}
 
-	public ArrayList<Card> return_cards() {
+	public ArrayList<Card> return_cards() { // To get the player's hands cards
 		return current_cards;
 	}
 
